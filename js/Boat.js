9a4322cbcfcbd2b9d1d6aeb0638761be04428522
Thays Silva <tplsilva05@gmail.com>
2024-02-14 17:00:50 -0300

    update

--- js/Boat.js
+++ js/Boat.js
@@ -1,24 +1,37 @@
 class Boat {
+  // Construtor da classe Boat
   constructor(x, y, width, height, boatPos) {
-  
+    // Cria um corpo retangular utilizando Matter.js
     this.body = Bodies.rectangle(x, y, width, height);
+    // Armazena as dimensões da embarcação como propriedades
     this.width = width;
     this.height = height;
-
+    // Carrega a imagem da embarcação
     this.image = loadImage("./assets/boat.png");
+    // Armazena a posição vertical relativa da imagem em relação ao corpo físico
     this.boatPosition = boatPos;
+    // Adiciona o corpo ao mundo físico
     World.add(world, this.body);
   }
 
+  // Método para exibir a embarcação
   display() {
+    // Obtém o ângulo de rotação do corpo físico
     var angle = this.body.angle;
+    // Obtém a posição do corpo físico
     var pos = this.body.position;
 
+    // Salva o estado atual da transformação
     push();
+    // Translada o sistema de coordenadas para a posição do corpo físico
     translate(pos.x, pos.y);
+    // Rotaciona o sistema de coordenadas pelo ângulo do corpo físico
     rotate(angle);
+    // Define o modo de exibição da imagem no centro
     imageMode(CENTER);
+    // Desenha a imagem da embarcação na posição relativa this.boatPosition
     image(this.image, 0, this.boatPosition, this.width, this.height);
+    // Restaura o estado original da transformação
     pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Constraint = Matter.Constraint;
 
 var engine, world, backgroundImg,boat;
 var canvas, angle, tower, ground, cannon;
+// Criando matrizes balls (balas) e boats (navios) vazias.
 var balls = [];
 var boats = [];
 
@@ -72,33 +73,46 @@ function showCannonBalls(ball, index) {
 }
 
 function showBoats() {
+  // Verifica se há barcos existentes no array 'boats'
   if (boats.length > 0) {
+    // Verifica se o último barco é indefinido ou se está à esquerda da largura da tela menos 300 pixels
     if (
       boats[boats.length - 1] === undefined ||
       boats[boats.length - 1].body.position.x < width - 300
     ) {
+      // Define posições possíveis para os novos barcos
       var positions = [-40, -60, -70, -20];
+      // Escolhe aleatoriamente uma posição do array 'positions'
       var position = random(positions);
+      // Cria um novo barco na parte direita da tela com base na posição escolhida aleatoriamente
       var boat = new Boat(width, height - 100, 170, 170, position);
 
+      // Adiciona o novo barco ao array 'boats'
       boats.push(boat);
     }
 
+    // Loop para iterar sobre todos os barcos existentes
     for (var i = 0; i < boats.length; i++) {
+      // Verifica se o barco atual existe (não é undefined)
       if (boats[i]) {
+        // Define a velocidade do barco para movê-lo para a esquerda
         Matter.Body.setVelocity(boats[i].body, {
           x: -0.9,
           y: 0
         });
 
+        // Exibe o barco na tela
         boats[i].display();
       } 
     }
   } else {
+    // Se não houver barcos, cria um novo barco na parte direita da tela
     var boat = new Boat(width, height - 60, 170, 170, -60);
+    // Adiciona o novo barco ao array 'boats'
     boats.push(boat);
   }
 }
+
 
 function keyReleased() {
   if (keyCode === DOWN_ARROW) {
